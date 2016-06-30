3a00fa970bea1b6cffc9c1aff24b2afaf50d7edc
Tommy Williams <tom@teow.co.uk>
2016-06-30 14:11:35 +0100

    added tests and modified code

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,16 @@
+require 'fizzbuzz'
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'fizz'
+  end
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns original number when passed 1' do
+    expect(fizzbuzz(1)).to eq 1
+  end
+end
